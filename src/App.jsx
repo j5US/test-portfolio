2923324b42b346ba91abadba4f5d70332c9faf56
Mriganka Paul <ironmaniscool99@gmail.com>
2024-12-23 22:09:21 +0530

    minor fix to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 // import DemoGrid from "./DemoGrid"
 // import NavBar from "./components/NavBar"
 import AboutPage from "./pages/AboutPage";
